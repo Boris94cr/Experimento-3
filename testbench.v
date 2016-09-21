1b773ea28d98a5afc0efb6a48930273c184f0f13
Boris94cr <baach94@hotmail.com>
2016-09-21 08:18:41 -0600

    ucf

---
+++ testbench.v
@@ -0,0 +1,29 @@
+module testbench;
+ 
+	reg Clock;
+	reg Reset;
+	wire oLCD_Enabled;
+	wire oLCD_RegisterSelect;
+	wire oLCD_StrataFlashControl;
+	wire oLCD_ReadWrite;
+	wire [3:0]oLCD_Data;
+	
+	always
+	#10 Clock = ~Clock;
+	
+	initial
+		begin
+			Clock = 0;
+			Reset = 1;
+			#50 Reset = 0;
+		end
+		
+	Module_LCD_Control LCD (
+		.Clock(Clock),
+		.Reset(Reset),
+		.oLCD_Enabled(oLCD_Enabled),
+		.oLCD_RegisterSelect(oLCD_RegisterSelect), //0=Command, 1=Data
+		.oLCD_StrataFlashControl(oLCD_StrataFlashControl),
+		.oLCD_ReadWrite(oLCD_ReadWrite),
+		.oLCD_Data(oLCD_Data)
+	);
